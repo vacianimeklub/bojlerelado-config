728ec77501d7779fb5656e36002e7042748cf1c7
Zoltán Adamek <djscorchio@gmail.com>
2019-07-06 21:38:26 +0200

    Try less complex setup without the CMS stuff

--- minecraft_server.sh
+++ minecraft_server.sh
@@ -6,10 +6,7 @@ for (( ; ; ))
 do
         java -Xmx${mcmemmb}M `: #              Max memory allocated` \
              -Xms${mcmemmb}M `: #              Initial memory allocated` \
-             -XX:+UseConcMarkSweepGC `: #      Oracle docs: "This collector should be considered for any application with a low pause time requirement."` \
              -XX:+UseParNewGC`: #              Oracle docs: "Uses a parallel version of the young generation copying collector alongside the default collector. This minimizes pauses by using all available CPUs in parallel."` \
-             -XX:+CMSIncrementalMode `: #      Enables incremental garbage collection` \
-             -XX:+CMSIncrementalPacing `: #    Enables automatic adjustment of the incremental mode duty cycle based on statistics collected while the JVM is running` \
              -XX:ParallelGCThreads=2 `: #      Sets the number of garbage collector threads` \
              -XX:+AggressiveOpts`: #           Enables the use of aggressive performance optimization features, which are expected to become default in upcoming releases` \
              -jar minecraft_server.jar nogui
